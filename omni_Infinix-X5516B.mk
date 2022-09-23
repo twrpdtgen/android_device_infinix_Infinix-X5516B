0131ab64648fa635a33794915465c973262bf2a0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-23 19:07:06 +0200

    Infinix-X5516B: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: infinix Model: Infinix X5516B Script version: 2.4.0

---
+++ omni_Infinix-X5516B.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Infinix-X5516B device
+$(call inherit-product, device/infinix/Infinix-X5516B/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Infinix-X5516B
+PRODUCT_NAME := omni_Infinix-X5516B
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X5516B
+PRODUCT_MANUFACTURER := infinix
